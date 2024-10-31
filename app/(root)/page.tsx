793ee7ae0de79b6dbe40f74e9053463ffbfd97ea
Greg <47268138+gregsypek@users.noreply.github.com>
2024-10-31 09:03:28 +0100

    add layout and basic styling for startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
 	searchParams,
@@ -6,6 +7,21 @@ export default async function Home({
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			// _createdAt: "Yesterday",
+			_createdAt: new Date(),
+			views: 44,
+			author: { _id: 1, name: "Greg" },
+			_id: 1,
+			description: "This is a description",
+			image: "https://loremflickr.com/200/200?random=1",
+			category: "Robots",
+			title: "We Robots",
+		},
+	];
+
 	return (
 		<>
 			<section className="pink_container">
@@ -17,6 +33,20 @@ export default async function Home({
 					Competitions.
 				</p>
 				<SearchForm query={query} />
+			</section>
+			<section className="section_container">
+				<p className="text-30-semibold">
+					{query ? `Search results for "${query}"` : "All Startups"}
+				</p>
+				<ul className="mt-7 card_grid">
+					{posts?.length > 0 ? (
+						posts.map((post: StartupCardType, index: number) => (
+							<StartupCard key={post?._id} post={post} />
+						))
+					) : (
+						<p className="no-results">No startups found</p>
+					)}
+				</ul>
 			</section>
 		</>
 	);
---
+++ components/StartupCard.tsx
@@ -0,0 +1,65 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+	const {
+		_createdAt,
+		views,
+		author: { _id: authorId, name },
+		_id,
+		description,
+		image,
+		category,
+		title,
+	} = post;
+
+	return (
+		<li className="startup-card group">
+			<div className="flex-between">
+				<p className="startup_card_date">{formatDate(_createdAt)}</p>
+				<div className="flex gap-1 5">
+					<EyeIcon className="size-6 text-primary" />
+					<span className="text-16-medium">{views}</span>
+				</div>
+			</div>
+			<div className="flex-between mt-5 gap-5">
+				<div className="flex-1">
+					<Link href={`/users/${authorId}`}>
+						<p className="text-16-medium line-clamp-1">{name}</p>
+					</Link>
+					<Link href={`/startup/${_id}`}>
+						<h3 className="text-26-semibold line-clamp-1">{title}</h3>
+					</Link>
+				</div>
+				<Link href={`/users/${authorId}`}>
+					<Image
+						src="https://placehold.co/48x48"
+						alt="plaeholder"
+						width={48}
+						height={48}
+						className="rounded-full"
+					/>
+				</Link>
+			</div>
+			<Link href={`/startup/${_id}`}>
+				<p className="startup-card_desc">{description}</p>
+				<img src={image} alt="placeholder" className="startup-card_img" />
+			</Link>
+			<div className="flex-between gap-3 mt-5">
+				<Link href={`/?query=${category.toLowerCase()}`}>
+					<p className="text-16-medium">{category}</p>
+				</Link>
+				{/* Tag asChild dla komponentu <Button> oznacza, że komponent ten przyjmuje inny element jako swojego "dziecka" i dziedziczy jego właściwości oraz styl. Jest to użyteczne w bibliotekach takich jak Radix UI, ponieważ pozwala nadpisać domyślny tag elementu (np. <button>) dowolnym elementem HTML (np. <a>, <div>) lub innym komponentem, zachowując przy tym funkcjonalność i styl komponentu <Button>. */}
+				<Button className="startup-card_btn" asChild>
+					<Link href={`/startup/${_id}`}>Details</Link>
+				</Button>
+			</div>
+		</li>
+	);
+};
+
+export default StartupCard;
